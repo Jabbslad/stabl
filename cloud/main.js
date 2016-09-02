e03fed2e5efa5e1037da2365f25440cbda4278a0
Jabbslad <jabbslad@gmail.com>
2016-09-02 22:03:48 +0100

    update cloud function

--- cloud/main.js
+++ cloud/main.js
@@ -1,19 +1,33 @@
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('hello world');
+Parse.Cloud.define('ping', function(req, res) {
+  res.success('pong!');
 });
 Parse.Cloud.define('random', function(req, res) {
 
-    Array.prototype.randomElement = function (min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+    function shuffle(a) {
+        var j, x, i;
+        for (i = a.length; i; i--) {
+            j = Math.floor(Math.random() * i);
+            x = a[i - 1];
+            a[i - 1] = a[j];
+            a[j] = x;
+        }
     }
 
     query = new Parse.Query("PodcastItem");
+    
+    if (req.params.genres) {
+        query.containedIn("genres", req.params.genres);
+    }
+
+    if (req.params.duration) {
+        query.lessThanOrEqualTo("enclosure.duration", req.params.duration);
+    }
 
     query.find({
         success: function(results) {
-            var index = results.randomElement(0, results.length);
-            console.log("random index [0-" + results.length + "] = " + index);
-            res.success(results[index]);
+            var max = results.length > 10 ? 10 : results.length;
+            shuffle(results);
+            res.success(fruits.slice(0, ma););
         },
         error: function(error) {
             res.error(error);
