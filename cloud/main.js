1bb48d922b7c1ae2989bb71714ab1a95f028e996
Jabbslad <jabbslad@gmail.com>
2016-09-02 15:29:50 +0100

    update cloud function

--- cloud/main.js
+++ cloud/main.js
@@ -2,13 +2,17 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('hello world');
 });
 Parse.Cloud.define('random', function(req, res) {
-    query = new Parse.Query("PodcastItem");
-    if (req.params.genres) {
-        query.containedIn("genres", req.params.genres)
+
+    Array.prototype.randomElement = function (min=0, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
+
+    query = new Parse.Query("PodcastItem");
+
     query.find({
         success: function(results) {
-            res.success(results);
+            var idx = randomElement(results.length);
+            res.success(results[idx]);
         },
         error: function(error) {
             res.error(error);
