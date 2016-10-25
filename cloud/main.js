13647481a85d40dc853ba29d7f4e61033730b596
Jabbslad <jabbslad@gmail.com>
2016-10-25 21:09:59 +0100

    update cloud function

--- cloud/main.js
+++ cloud/main.js
@@ -30,7 +30,7 @@ Parse.Cloud.define('random', function(req, res) {
     }
 
     if (req.params.pubDate) {
-        query.greaterThanOrEqualTo("pubDate", req.params.pubDate);
+        query.greaterThanOrEqualTo("pubDate", Date.parse(req.params.pubDate));
     }
 
     query.find({
